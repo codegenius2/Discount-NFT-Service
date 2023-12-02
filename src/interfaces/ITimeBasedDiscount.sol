ccb3234af46dc2e0c0ebaa32a7d107d207fb6283
codegenius2 <lucifer231220@gmail.com>
2023-12-03 00:04:14 +0330

    Add mint timeBased discount interface

--- src/interfaces/ITimeBasedDiscount.sol
+++ src/interfaces/ITimeBasedDiscount.sol
@@ -6,5 +6,9 @@ interface ITimeBasedDiscount {
 
     function initialize(string calldata tokenName, string calldata tokenSymbol) external;
 
-    
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) external;
+
+    function createToken(string calldata newUri, uint64 startAt, uint64 endAt, uint64 ratio) external;
+
+
 }
