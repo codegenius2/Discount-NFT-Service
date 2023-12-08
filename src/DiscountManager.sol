32f7b709e948364eda9b88c5c1938eb8eb66441d
codegenius2 <lucifer231220@gmail.com>
2023-12-08 17:41:39 +0330

    inherit source manager

--- src/DiscountManager.sol
+++ src/DiscountManager.sol
@@ -7,95 +7,99 @@ import { Initializable } from "@openzeppelin/contracts-upgradeable@4.9.1/proxy/u
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable@4.9.1/proxy/utils/UUPSUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable@4.9.1/access/OwnableUpgradeable.sol";
 import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable@4.9.1/security/PausableUpgradeable.sol";
+import { SourceManager } from "./CrossChainUtils/SourceManager.sol";
 
+contract DiscountManager is Initializable, OwnableUpgradeable, PausableUpgradeable, UUPSUpgradeable, SourceManager {
 
-contract DiscountManager is Initializable, OwnableUpgradeable, PausableUpgradeable, UUPSUpgradeable {
-
+    enum Types { Inactive, TimeBased, StaticBased }
 
     struct Discount {
         address owner;
-        address timeBased;
-        address staticBased;
+        Types discountType; 
     }
-    
-    address public ccipRouter;
-
-    uint64 chainSelector;
-    address public bcsReceiver;
 
     mapping(bytes32 => Discount) public nameToDiscount;
+    mapping(bytes32 => uint256[]) internal discountChainIds;
+
     mapping(bytes32 => mapping(address => uint256)) public discountToClaimBalance;
 
-    event MessageSent(bytes32 messageId);
+    event DiscountInitialized(address owner, string discountName, Types discountType);
 
-
-    function initialize(address _ccipRouter, uint64 _chainSelector, address reveiver) initializer public {
+    function initialize(address _ccipRouter) initializer public {
         __Ownable_init();
         __Pausable_init();
         __UUPSUpgradeable_init();
-        ccipRouter = _ccipRouter;
-        chainSelector = _chainSelector;
-        bcsReceiver = reveiver;
+        __SourceManager_init(_ccipRouter);
     }
 
 
-    function createStaticDiscount(string memory tokenName, string memory tokenSymbol, uint256[] memory tokenIds, string[] memory uris) public payable {
+    function initializeDiscount(string calldata tokenName, Types tokenType) public {
+
+        require(uint8(tokenType) == 1 || uint8(tokenType) == 2, "Invalid token type");
 
         require(bytes(tokenName).length <= 32, "Token name is too long");
         bytes32 name = getBytesString(tokenName);
 
-        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
-            receiver: abi.encode(bcsReceiver),
-            data: abi.encodeWithSignature(
-                "createStaticDiscount(string,string,uint256[],string[])", 
-                tokenName, tokenSymbol, tokenIds, uris
-            ),
-            tokenAmounts: new Client.EVMTokenAmount[](0),
-            extraArgs: Client._argsToBytes(
-                Client.EVMExtraArgsV1({gasLimit: 4_000_000})
-            ),
-            feeToken: address(0)
-        });
+        Discount storage discount = nameToDiscount[name];
+        require(discount.discountType == Types.Inactive, "Token already initialized");
 
-        uint256 fee = IRouterClient(ccipRouter).getFee(
-            chainSelector,
-            message
-        );
+        discount.owner = msg.sender;
+        discount.discountType = tokenType;
 
-        require(msg.value >= fee, "Insufficient value");
-
-        bytes32 messageId = IRouterClient(ccipRouter).ccipSend{value: fee}(
-            chainSelector,
-            message
-        );
-        
-        emit MessageSent(messageId);
-
-    
+        emit DiscountInitialized(msg.sender, tokenName, tokenType);
     }
+
+
+    // function createStaticDiscount(string memory tokenName, string memory tokenSymbol, uint256[] memory tokenIds, string[] memory uris, uint256 chainId) public payable {
+
+    //     require(bytes(tokenName).length <= 32, "Token name is too long");
+    //     bytes32 name = getBytesString(tokenName);
+
+    //     //Discount storage discount = nameToDiscount[name];
+
+
+    //     // if(discount.discountType == Types.Inactive) {
+    //     //     discount.owner = msg.sender;
+    //     //     discount.discountType = Types
+    //     // }
+
+        
+
+    //     bytes memory data = abi.encodeWithSignature(
+    //         "createStaticDiscount(string,string,uint256[],string[])", 
+    //         tokenName, tokenSymbol, tokenIds, uris
+    //     );
+
+    //     // create ccip message to send the receive contract
+    //     Client.EVM2AnyMessage memory message = _buildCCIPMessage(data, chainId);
+
+    //     // check the caller send enogh msg value for ccip
+    //     uint256 fee = _getCCIPMessageFee(message, chainId);
+    //     require(msg.value >= fee, "Insufficient value");
+
+    //     // send the message and create the discount
+    //     _sendCCIPMessage(message, chainId);
+    
+    // }
+
+
+
 
 
     function getCreateStaticDiscountFee(
         string calldata tokenName, string calldata tokenSymbol, 
-        uint256[] calldata tokenIds, string[] calldata uris) public view returns(uint256 fee) {
-            
-        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
-            receiver: abi.encode(bcsReceiver),
-            data: abi.encodeWithSignature(
-                "createStaticDiscount(string,string,uint256[],string[])", 
-                tokenName, tokenSymbol, tokenIds, uris
-            ),
-            tokenAmounts: new Client.EVMTokenAmount[](0),
-            extraArgs: Client._argsToBytes(
-                Client.EVMExtraArgsV1({gasLimit: 4_000_000})
-            ),
-            feeToken: address(0)
-        });
-
-        fee = IRouterClient(ccipRouter).getFee(
-            chainSelector,
-            message
+        uint256[] calldata tokenIds, string[] calldata uris, uint256 chainId) public view returns(uint256 fee) {
+        
+        bytes memory data = abi.encodeWithSignature(
+            "createStaticDiscount(string,string,uint256[],string[])", 
+            tokenName, tokenSymbol, tokenIds, uris
         );
+
+        // create ccip message to send the receive contract
+        Client.EVM2AnyMessage memory message = _buildCCIPMessage(data, chainId);
+
+        // returns the fee for sending the ccip message
+        fee = _getCCIPMessageFee(message, chainId);
     }
 
 
@@ -105,7 +109,7 @@ contract DiscountManager is Initializable, OwnableUpgradeable, PausableUpgradeab
      * @param name The input string to be converted.
      * @return _name The resulting bytes32 value.
      */
-    function getBytesString(string memory name) internal pure returns(bytes32 _name) {
+    function getBytesString(string memory name) private pure returns(bytes32 _name) {
         assembly {
             _name := mload(add(name, 32))
         }
