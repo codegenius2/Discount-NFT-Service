ff7bf92b7e6d15997022e68c93975e21f9e79f40
codegenius2 <lucifer231220@gmail.com>
2023-12-11 03:05:22 +0330

    Fixed params memory location

--- src/interfaces/ITimeBasedDiscount.sol
+++ src/interfaces/ITimeBasedDiscount.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.21;
 
 interface ITimeBasedDiscount {
 
-    function initialize(string calldata tokenName, string calldata tokenSymbol) external;
+    function initialize(string memory tokenName, string memory tokenSymbol) external;
 
     function mint(address account, uint256 id, uint256 amount, bytes memory data) external;
 
-    function createToken(string calldata newUri, uint64 startAt, uint64 endAt, uint64 ratio) external;
+    function createToken(string memory newUri, uint64 startAt, uint64 endAt, uint64 ratio) external;
 
 
 }
